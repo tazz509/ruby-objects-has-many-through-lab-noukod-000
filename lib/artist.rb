68ced92db2c456c616661e709e4b7ee355e98546
carly <carly@noukod.com>
2018-10-15 15:24:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,26 @@
+require 'pry'
 class Artist
-  attr_accessor :name
-
-  @@all = []
-
-  def initialized
+  attr_accessor :name, :artist, :genre
+  ALL=[]
+  def initialize(name)
     @name = name
-    @all << self
+    ALL << self
   end
-
-def self.all?
-  @all
-end
-
-def new_song
-end
-
-def songs
-end
-
-def genres
-end
+  def self.all
+    ALL
+  end
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
 end
