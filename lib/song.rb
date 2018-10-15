e144fb39eec87efa2c167968fed5a85728e037a1
carly <carly@noukod.com>
2018-10-15 15:29:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,23 @@
 require 'pry'
+
 class Song
+  
   attr_accessor :name, :artist, :genre
-  ALL = []
+  all = []
+  
   def initialize(name="", artist="", genre="")
     @name = name
     @artist = artist
     @genre = genre
-    ALL << self
+    all << self
   end
+  
   def self.all
-    ALL
+    all
   end
+  
   def artist
     @artist
   end
+  
 end
